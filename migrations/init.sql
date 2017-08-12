301577c51cbee4ae782a1080e0cbeccaba7cbf15
SimonIsMe <szym1991@gmail.com>
2017-08-12 11:26:17 +0200

    Fix using migration

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,7 +1,9 @@
 CREATE TABLE users (
+    id int NOT NULL AUTO_INCREMENT,
     email varchar(255) NOT NULL,
     name varchar(255) NOT NULL,
-    is_active BOOLEAN default true NOT NULL
+    is_active BOOLEAN default true NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE UNIQUE INDEX projects_users_unique_index
